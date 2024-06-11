805fd0d0aa8027eaf7fca79efbfa90eb813108bf
Hassan Khokhar <67870445+mhassan7599@users.noreply.github.com>
2024-06-11 10:45:57 +0500

    comment

---
+++ assets/js/custom.js
@@ -0,0 +1,22 @@
+let offset = $(".header-wrapper").height();
+				
+$(document).on("click", "a[href*=#]:not([href=#])", function(event){
+    let hash = this.hash;
+    let $target = $(hash);
+      
+   if($target.length){
+        $("html,body").stop().animate({
+            scrollTop: $target.offset().top - offset
+        }, 1000, function(){
+            if(history.pushState) {
+              history.pushState(null, null, hash);
+            }
+        });
+                      
+      event.preventDefault();
+  }
+  
+  $( 'html, body' ).animate({
+    scrollTop: $( href ).offset().top
+}, '300' );
+});
